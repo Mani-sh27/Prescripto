29f049059489edf3596914d0cb3223df9a40aa34
Mani-sh27 <beingahuman2512gmail.com>
2025-08-31 22:32:50 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -10,7 +10,7 @@ const TopDoctors = () => {
         {
           doctors.slice(0, 10).map((item, index) => (
             <div className=' size-50  mx-auto  cursor-pointer  hover:-translate-y-2 duration-500 transition-all'>
-              <img className='bg-blue-50  rou' src={item.image} alt="" />
+              <img className='bg-blue-50  rounded-2xl' src={item.image} alt="" />
               <div>
                 <p className='ms-8'>Available</p>
               </div>
