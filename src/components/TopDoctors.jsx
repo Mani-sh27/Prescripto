d63aa88450aa6b6e34a96686aaa8b0b0d6a8b171
Mani-sh27 <beingahuman2512gmail.com>
2025-08-31 23:07:27 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -9,7 +9,7 @@ const TopDoctors = () => {
       <div className='grid w-full grid-cols-auto'>
         {
           doctors.slice(0, 10).map((item, index) => (
-            <div className=''>
+            <div className='mx'>
               <div className='  mt-6  w-68 mx-auto cursor-pointer border-blue-100 border-2 rounded-2xl hover:-translate-y-2 duration-500 transition-all'>
                 <img className='bg-blue-50  rounded-t-2xl' src={item.image} alt="" />
                 <div>
