82a471e4bbe36f29e18ff58d60aa8687da652369
Mani-sh27 <beingahuman2512gmail.com>
2025-09-01 21:58:06 +0530

    auto commit

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    AppContextProvider
+    <AppContextProvider>
+      
+    </AppContextProvider>
     <App />
     </BrowserRouter>
   </StrictMode>,
