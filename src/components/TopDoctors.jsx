e1917d63f22ad804d46481b06121249cef277028
Mani-sh27 <beingahuman2512gmail.com>
2025-09-02 00:08:58 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { assets, doctors } from '../assets/assets_frontend/assets'
 import { useNavigate } from 'react-router-dom';
-import {  } from "module";
+import {  } from "";
 
 const TopDoctors = () => {
 
