08c4bd62c1373cb9c693f7e211c727c091949f83
Mani-sh27 <beingahuman2512gmail.com>
2025-09-01 17:35:29 +0530

    auto commit

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,7 +1,5 @@
 import { createContext } from "react";
 import { doctors } from "../assets/assets_frontend/assets";
-
-
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
