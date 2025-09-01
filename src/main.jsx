9f1efb0531eb9f595e34d16363392bb80b87aca2
Mani-sh27 <beingahuman2512gmail.com>
2025-09-02 00:05:51 +0530

    auto commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import './App.css'
 import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
-import App
+import AppC
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
