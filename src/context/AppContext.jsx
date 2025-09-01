399bf2637cf31b44bb72fc16e85ab7be3e852ec1
Mani-sh27 <beingahuman2512gmail.com>
2025-09-01 17:33:32 +0530

    auto commit

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import { doctors } from "../assets/";
+import { doctors } from "../assets/as";
 import { BrowserRouter } from "react-router-dom";
 
 export const AppContext = createContext()
