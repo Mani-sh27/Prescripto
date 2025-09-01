cf94c9fe49d10432a5bf803c0bdb140660580fa0
Mani-sh27 <beingahuman2512gmail.com>
2025-09-01 23:57:41 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -30,7 +30,7 @@ const TopDoctors = () => {
           ))
         }
       </div>
-      <button className='bg-blue-50' onc>more</button>
+      <button className='bg-blue-50' oncli>more</button>
     </div>
   )
 }
