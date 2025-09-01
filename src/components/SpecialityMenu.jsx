4fa88442d56ae56b349b0460498866bf52c246e9
Mani-sh27 <beingahuman2512gmail.com>
2025-09-01 23:12:19 +0530

    auto commit

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { specialityData } from '../assets/assets_frontend/assets';
 import {Link} from 'react-router-dom';
 const SpecialityMenu = () => {
-  const navigate = use
+  const navigate = useContext
 
   return (
     <div id='speciality' >
