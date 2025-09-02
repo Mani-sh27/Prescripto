84c1e86e02b4f9cb2fe7ffee70230ba624eabf5f
Mani-sh27 <beingahuman2512gmail.com>
2025-09-02 07:41:59 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,8 @@ const [token , setToken] = useState(true)
             token ? <div >
               <img className='w-9 rounded-full m-' src={assets.profile_pic} alt="" /> 
               <img src={assets.dropdown_icon} alt="" />
-              </div>:<button className='me-20 bg-blue-400 rounded-full px-8 cursor-pointer '>Create Account</button>
+              </div>
+              :<button className='me-20 bg-blue-400 rounded-full px-8 cursor-pointer '>Create Account</button>
 
           }
       
