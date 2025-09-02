7469d52fdda70089cdc943d282b9e0f37ff27823
Mani-sh27 <beingahuman2512gmail.com>
2025-09-02 07:43:25 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,9 @@ const Navbar = () => {
             <div >
               <img className='w-9 rounded-full m-' src={assets.profile_pic} alt="" />
               <img src={assets.dropdown_icon} />
-              div
+              <div>
+                
+              </div>
             </div>
             : <button className='me-20 bg-blue-400 rounded-full px-8 cursor-pointer '>Create Account</button>
 
