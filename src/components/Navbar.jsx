2fb44161f1d4b5976f405e9b16e082d252f0bfac
Mani-sh27 <beingahuman2512gmail.com>
2025-09-02 07:19:13 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const [token , setToken] = useState(true)
           </NavLink>
         </ul>
           {
-            token ? <div></div>:<Bu></Bu>
+            token ? <div></div>:<button></button>
           }
         <button className='me-20 bg-blue-400 rounded-full px-8 cursor-pointer '>Create Account</button>
 
