2328973f69724acb501cf6010b65eacff4e61669
Mani-sh27 <beingahuman2512gmail.com>
2025-09-02 07:43:50 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             <div >
               <img className='w-9 rounded-full m-' src={assets.profile_pic} alt="" />
               <img src={assets.dropdown_icon} />
-              <div class>
+              <div classn>
 
               </div>
             </div>
