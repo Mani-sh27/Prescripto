a44c490765ff8d1f72ea0494174aa7c0e17289c2
Mani-sh27 <beingahuman2512gmail.com>
2025-09-05 18:21:59 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
                 <div className='flex flex-col gap-28 mt-4'>
                     <img className='relative top-40  w-24 ms-16 mt-5 ' src={assets.group_profiles} alt="" />
                     <p className='text-white ms-44'>Simply browse through our extensive list of trusted doctors. <br /> scheduled your appointment hassle-free </p>
-                    <a className='relative -top-24 left-24 pt-2 ps-6 -pt-4 rounded-full  bg-white w-56 hover:transition-all translate-y-2 duration-300' href="">Book Appointment<img className='relative left-38 -top-4.5 ' src={assets.arrow_icon} /></a>
+                    <a className='relative -top-24 left-24 pt-2 ps-6 -pt-4 rounded-full  bg-white w-56 hover: translate-y-2 duration-300' href="">Book Appointment<img className='relative left-38 -top-4.5 ' src={assets.arrow_icon} /></a>
                 </div>
             </div>
             <img src={assets.header_img}  />
