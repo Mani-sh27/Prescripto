42263fb1b9051310b39451c3607dcfbf7e3214af
Mani-sh27 <beingahuman2512gmail.com>
2025-09-05 21:45:51 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
         <div>
             <p>Book Appointment</p>
             <p>with 100+ Truste Doctor</p>
-            <button>Create account</button>
+            <button className=''>Create account</button>
         </div>
         <div className=''>
             <img  className='w-68' src={assets.appointment_img}  />
