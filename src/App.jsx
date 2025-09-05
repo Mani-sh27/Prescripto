775b8b4dd526f366f1785ee890238d5f1654fdb3
Mani-sh27 <beingahuman2512gmail.com>
2025-09-05 18:10:23 +0530

    auto commit

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
       <Routes>
         <Route path="/" element={
           <>
-       =
+       ==
             <Header />
             <SpecialityMenu />
             <TopDoctors />
