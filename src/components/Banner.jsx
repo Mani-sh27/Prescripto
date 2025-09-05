83c805a197efd3e2fd69e8034b2bad03c684c63c
Mani-sh27 <beingahuman2512gmail.com>
2025-09-05 21:42:32 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
             <p>with 100+ Truste Doctor</p>
         </div>
         <div>
-            <img src={assets.appointment_img}  />
+            <img  classrc={assets.appointment_img}  />
         </div>
     </div>
   )
