c8f8f5ed564eeb1c68f5a51d1c804a91bb551fe9
Mani-sh27 <beingahuman2512gmail.com>
2025-09-05 21:36:48 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div>
         <div>
-            <p>Book Appointment with 100+ Truste Doctor</p>
+            <p>Book Appointmentr</p>
             <p></p>
         </div>
         <div>
