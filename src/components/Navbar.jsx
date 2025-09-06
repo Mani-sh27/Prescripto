75db8c70a3b45bace965c7ef5368cc96d5ed1bdd
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 00:05:51 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             <NavLink className={({isActive})=>`pb-1 ${isActive ? 'border-none border-b-2' : ''}`} to='/doctors'>
               <li className='font-semibold'>ALL DOCTORS</li>
             </NavLink>
-            <NavLink className={({isActive})=>`pb-1${isActive ? 'border-b-2' : ''}`} to='/about'>
+            <NavLink className={({isActive})=>`pb-1${isActive ? ' border-b-2' : ''}`} to='/about'>
               <li className='font-semibold'>ABOUT</li>
             </NavLink>
             <NavLink className={({isActive})=>`pb-1${isActive ? '' : ''}`} to='/contact'>
