6db4dbcce80ee0178f2e03747412033853a4d9d1
Mani-sh27 <beingahuman2512gmail.com>
2025-09-06 23:16:16 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,18 +1,8 @@
 import React from 'react'
-import { assets } from '../assets/assets_frontend/assets'
 
 const Banner = () => {
   return (
-    <div className='flex'>
-        <div>
-            <p>Book Appointment</p>
-            <p>with 100+ Truste Doctor</p>
-            <button className=''  type='button'>Create account</button>
-        </div>
-        <div className='md-block hidden'>
-            <img  className='w-68' src={assets.appointment_img}  />
-        </div>
-    </div>
+    <div>Banner</div>
   )
 }
 
