078239df23f53745d564308869e260f9c31f6745
Mani-sh27 <beingahuman2512gmail.com>
2025-09-06 16:45:19 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets_frontend/assets'
 const Header = () => {
   return (
     <div className='flex justify-between lg:w-7xl bg-[#5f6fff] sm:flex-col flex mt-6 rounded-lg 
-                      lg:mx-28 '>
+                     '>
       <div className=' text-center '>
         <h1 className='font-semibold text-white relative top-40 left-16 xs:flex text-3xl'>
           Book Appointment <br />with Trusted Doctors
