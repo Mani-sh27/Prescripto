43aa8a28c63912c44af60617262d1e53a2321179
Mani-sh27 <beingahuman2512gmail.com>
2025-09-06 23:35:46 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <div>
           <ul>
             <NavLink to='/'>
-              <li className='/ab'></li>
+              <li className='/about'></li>
             </NavLink>
           </ul>
         </div>
