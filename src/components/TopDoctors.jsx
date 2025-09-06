05b37073f4b30de0b5e3be802ca956c2c99362f4
Mani-sh27 <beingahuman2512gmail.com>
2025-09-06 22:30:19 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -12,7 +12,7 @@ const TopDoctors = () => {
     <div>
       <h1 className='text-4xl mt-20  text-center font-semibold'>Top Doctors</h1>
       <p className='text-center mt-4'>Simple Browse  through our extensive list of trusted doctors</p>
-      <div className='grid grid-cols-2 sm:grid-cols-5 grid-rows-2 lg:flex grid-cols-3 '>
+      <div className='grid grid-cols-2 sm:grid-cols-5 grid-rows-2 lg:flex grid-cols-3'>
         {
           doctors.slice(0, 10).map((item, index) => (
             <div className='p-6'>
