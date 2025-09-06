3c2354ab3441493d2317deed7b260bb0b0b7b059
Mani-sh27 <beingahuman2512gmail.com>
2025-09-06 23:11:11 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,8 +9,18 @@ const Navbar = () => {
 
   return (
     
-      
-  );
+        
+          ) : (
+            <button className='me-20 bg-blue-400 rounded-full px-8 cursor-pointer mx-auto sm:mx-0 py-2 text-sm sm:text-base'>
+              Create Account
+            </button>
+          )
+        }
+      </div>
+
+      <hr className=' sm:flex mx-12' />
+    </header>
+  )
 }
 
 export default Navbar
