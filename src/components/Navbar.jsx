8b40205b8296943be8c7b154d8a7b12926a1495d
Mani-sh27 <beingahuman2512gmail.com>
2025-09-06 23:49:25 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <header>
-      <div className='flex justi'>
+      <div className='flex justify-between'>
         <div>
             <img src={assets.logo} alt="" />
         </div>
