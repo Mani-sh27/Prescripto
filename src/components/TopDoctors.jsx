e67c08a212252e38ba44da4f35472109d8c501b4
Mani-sh27 <beingahuman2512gmail.com>
2025-09-06 23:09:59 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -9,14 +9,7 @@ const TopDoctors = () => {
    const navigate = useNavigate();
   const {doctors} = useContext(AppContext)
   return (
-    <div>
-      
-
-          ))
-        }
-      </div>
-      <button className='bg-blue-50 lg:ms-[650px] p-5 mt-4 cursor-pointer  rounded-full w-48 sm:flex ms-14 ' onClick={()=>{navigate('/doctors'); window.scroll(0,0)}}>more</button>
-    </div>
+  
   )
 }
 
