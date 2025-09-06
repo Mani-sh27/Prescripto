f325bdc5945a5249ede93a18bf46bbfdd5397d09
Mani-sh27 <beingahuman2512gmail.com>
2025-09-06 16:41:18 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
           >
             Book Appointment
             <img
-              className='relative -top-4.5  mx-auto md:mx-0'
+              className='relative -top-4.5 ms mx-auto md:mx-0'
               src={assets.arrow_icon}
               alt=''
             />
