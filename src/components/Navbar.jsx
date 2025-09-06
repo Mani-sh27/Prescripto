8fa6ea5d37f0ed414857f8532a558db0d2149dea
Mani-sh27 <beingahuman2512gmail.com>
2025-09-06 20:51:03 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
         {/* Profile or Button */}
         {
           token ? (
-            <div className=' gap-2 flex lg:-left-34 sm:flex relative -left-'>
+            <div className=' gap-2 flex lg:-left-34 sm:flex relative -left-52'>
               <img className='w-9 rounded-full ' src={assets.profile_pic} alt="" />
               <img className='w-4 cursor-pointer ' src={assets.dropdown_icon} alt="" />
               <div className='hidden'>
