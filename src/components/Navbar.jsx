b2d916c29af283cd13c6301cee22431447ff5d6c
Mani-sh27 <beingahuman2512gmail.com>
2025-09-06 23:11:33 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,1 @@
-import React, { useState } from 'react'
-import { assets } from '../assets/assets_frontend/assets'
-import { NavLink, useNavigate } from "react-router-dom";
-
-const Navbar = () => {
-  const navigate = useNavigate();
-  const [showMenu, setShowMenu] = useState(false);
-  const [token, setToken] = useState(true);
-
-  return (
-    
-        
-          ) 
-}
-
-export default Navbar
+r
