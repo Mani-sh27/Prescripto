9d7a43d910969f78b73eb85841fffa3cfd76d9bd
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 00:11:22 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <img src={assets.logo} alt="" />
         </div>
         <div>
-          <ul className='flex mt-4 gap-10 -ms-32 '>
+          <ul className='flex mt-4 gap-10 -ms-32 tex'>
             <NavLink className={({isActive})=>`pb-1 ${isActive ? ' border-b-2': '' }`} to='/'>
               <li className='font-semibold'>HOME</li>
             </NavLink>
