ed89e272e4b9ec05e7336cd366b8eafd8cdb3bd3
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 15:08:10 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
                 token ? <div>
                   <img src={assets.profile_pic} alt="" />
                   <img src={assets.dropdown_icon} alt="" />
-                </div> : <button onClick={()=>Navigate('')}>Create Account</button>
+                </div> : <button onClick={()=>Navigate('/')}>Create Account</button>
               }
 
             </div>
