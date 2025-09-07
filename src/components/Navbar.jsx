be18af823a1af5d43a022ca858cac8e5b0ccb193
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 14:56:17 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
             <div className='flex'>
               {
                 token ? <div>
-                  
+                  <img src="" alt="" />
                 </div> :
               }
 
