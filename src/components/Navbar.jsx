cc0b9b317b0bba53cb9741cfa4006fe844359c0c
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 06:44:48 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div>
-          <button type='button' className='border cursor '>Create account</button>
+          <button type='button' className='border cursor-pointer '>Create account</button>
         </div>
         <div>
 
