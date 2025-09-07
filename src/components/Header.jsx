53a2fbd7992b1c512d6ad23da126b0a0332dfe58
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 20:37:24 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <section>
         <p>Book Appointment <br />
           With Trusted Doctors</p>
-          <img src={} alt="" />
+          <img src={assets} alt="" />
       </section>
       <section>
 
