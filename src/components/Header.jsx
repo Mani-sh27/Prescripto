79611c3b777d46a124e30f5bd954c8957a2c1410
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 19:44:26 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets_frontend/assets'
 const Header = () => {
   return (
-    <div className=''>
+    <div className='fle'>
       <section>
         <h2></h2>
       </section>
