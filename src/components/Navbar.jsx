a6c9315ce970f08ca820c989934a609ee8a9a87c
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 15:49:20 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
                   <div>
                     <div>
                       <p>My Profile</p>
-                      <p></p>
+                      <p>My-Appoint</p>
                       <p></p>
                     </div>
                   </div>
