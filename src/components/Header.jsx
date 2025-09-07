7165df5768214c5043e4aea2d8c024e1e2a5cf89
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 21:33:34 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,8 @@ const Header = () => {
 
       </section>
       <section>
-        <img src={assets.header_img} className=' flex-col flex  relative w-[500px]   ' />
+        <img src={assets.header_img} className=' flex-col flex sm
+         relative w-[500px]   ' />
       </section>
     </div>
   )
