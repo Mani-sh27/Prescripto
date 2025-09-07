86293aa5e576ab0c465934d92128c3eda5102cd1
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 06:44:22 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div>
-          <button type='button'>Create </button>
+          <button type='button'>Create account</button>
         </div>
         <div>
 
