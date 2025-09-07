55f69a95482ee1500c53447ace894167b8e1e13b
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 21:07:26 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
           <img src={assets.group_profiles} alt="" />
           <p className='ms-4'>Simply browse through our extensive list of trusted doctors, <br />
             schedule your appointment hassle-free.</p>
-            <button type='button'>Book </button>
+            <button type='button'>Book appointment</button>
         </div>
 
       </section>
