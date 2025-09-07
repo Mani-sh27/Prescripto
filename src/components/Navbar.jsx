462595deecf3f32e4ee1b5fa7dc5f4d92daea909
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 06:38:49 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
               <li className='font-semibold text-sm'>CONTACT</li>
             </NavLink>
              <NavLink to='/admin'>
-              <li className='font- '>Admin Panel</li>
+              <li className=' '>Admin Panel</li>
             </NavLink>
           </ul>
         </div>
