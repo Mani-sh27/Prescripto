ed66c26b5e12680837a8f95890b7444f4f8af822
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 15:30:59 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
                   <img src={assets.profile_pic} className='rounded-full me-2 ' alt="" />
                   <img src={assets.dropdown_icon} alt="" />
                 </div> : <div className='hidden sm:block'>
-                  <button type='button' className='border cursor-pointer mt-7 px-5 bg-blue-500 text-white py-2 rounded-full '>Create account</button>
+                  <button type='button' className='border cursor-pointer mt-7 px-5 bg-blue-500 text-white py-2 rounded-full ms-10'>Create account</button>
                 </div>
               }
 
