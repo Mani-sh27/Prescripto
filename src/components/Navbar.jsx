d512e8ef5c6fce0c50bc93e8f25455abe51e8725
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 14:28:27 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
           </ul>
           
         </div>
-        <div className='hidden'>
+        <div className='hidden md'>
           <button type='button' className='border cursor-pointer mt-7 px-5 bg-blue-500 text-white py-2 rounded-full ms-10'>Create account</button>
         </div>
       </div>
