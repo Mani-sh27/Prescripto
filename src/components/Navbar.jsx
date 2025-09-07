6b41b5e598b368241162a663101148f581df5810
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 07:03:04 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
         <div>
           <button type='button' className='border cursor-pointer mt-7 px-5 bg-blue-500 text-white py-2 rounded-full ms-10'>Create account</button>
         </div>
-        <hr className='sm:flex pb-10 pb-' />
+        <hr className='sm:flex pb-10 pb-10' />
         <div>
 
         </div>
