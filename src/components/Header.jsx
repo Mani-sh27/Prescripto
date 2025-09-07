10075730e5f23d7bad295d41317b7d4ec40793a6
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 22:11:39 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
 
       </section>
       <section>
-        <img src={assets.header_img} className='w- ' />
+        <img src={assets.header_img} className='w-80 ' />
       </section>
     </div>
   )
