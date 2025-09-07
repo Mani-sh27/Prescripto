2c047cf54d78d902997934b0a9a78f18adfb0e86
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 21:05:47 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,6 @@ const Header = () => {
           <img src={assets.group_profiles} alt="" />
           <p>Simply browse through our extensive list of trusted doctors, <br />
             schedule your appointment hassle-free.</p>
-            b
         </div>
 
       </section>
