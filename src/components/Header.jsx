dc0ddc7b6f6eecae2f50f3bd2f3c9c95608acba1
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 23:00:55 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { assets } from '../assets/assets_frontend/assets'
 const Header = () => {
   return (
-    <div className='lg:flex-row sm:flex flex-col bg-[#5f6fff] rounded-xl mx-42 mt-4 sm'>
+    <div className='lg:flex-row sm:flex flex-col bg-[#5f6fff] rounded-xl mx-42 mt-4 sm:'>
       <section className=''>
         <p className='sm:text-5xl text-white relative left-20 font-semibold top-30 '>Book Appointment <br />
           With Trusted Doctors</p>
