83ff9efbbd0a2509e402510ad54271e80821eabd
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 14:54:30 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
              <NavLink to='/admin'>
               <li className='border px-3 py-1 rounded-full -mt-1 font-semibold text-xs transition-all duration-700 hover:bg-black hover:text-white '>Admin Panel</li>
             </NavLink>
-            <div className='flex '>
+            <div className='flex'>
 
             </div>
           </ul>
