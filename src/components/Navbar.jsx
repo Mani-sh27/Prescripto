737972522a9c9daf36dd534b26a3e06c59adf193
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 16:05:24 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
                   <img src={assets.dropdown_icon} />
                   <div className='absolute top-0 right-0 pt-14  w-[200px] '>
                     <div className='bg-gray-100 px-8 py-6 leading-7 '>
-                      <ul className='-ms-5'>
+                      <ul className='-ms-4'>
                         <p className=''>My Profile</p>
                         <p className=''>My-Appointments</p>
                         <p className=''>LogOut</p>
