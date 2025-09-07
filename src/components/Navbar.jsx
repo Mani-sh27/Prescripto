e66efc7cbb8a79003f6072a473bb370c3951fd19
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 07:58:23 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div>
             <img className=' mt-9 text-2xl  sm:mt-6 min-w-45' src={assets.logo} alt="" />
         </div>
-        <button className='smbg-blue-400'>
+        <button className='sm: flbg-blue-400'>
         njxfnjd
         </button>
         <div>
