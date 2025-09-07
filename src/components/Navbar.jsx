12cd27dcd6a8d55e5708c839c49f4cd8a407aa55
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 14:38:12 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         {open ? '✖': '☰'}
         </button>
         <div>
-          <ul className=' mt-10 gap-10 ms-28  sm:flex hidden '>
+          <ul className=' mt-10 gap-10 ms-28  sm:flex hidden mx-'>
             <NavLink className={({isActive})=>`pb-1 ${isActive ? ' border-b-2': '' }`} to='/'>
               <li className='font-semibold text-sm'>HOME</li>
             </NavLink>
