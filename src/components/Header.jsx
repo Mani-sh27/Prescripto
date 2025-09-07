6d5d234ef0def3a745aa7d52c7d0b4e443844fea
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 21:46:38 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,35 @@
 import React from 'react'
 import { assets } from '../assets/assets_frontend/assets'
+
 const Header = () => {
   return (
-    <div className='flex w-min-80 bg-[#5f6fff] rounded-xl mx-42 mt-4 h-[500px] '>
-      <section className=''>
-        <p className='sm:text-5xl text-white relative left-20 font-semibold top-30 '>Book Appointment <br />
-          With Trusted Doctors</p>
-        <div className='flex  relative top-38 text-white left-20'>
-          <img src={assets.group_profiles} alt="" />
-          <p className='ms-4'>Simply browse through our extensive list of trusted doctors, <br />
-            schedule your appointment hassle-free.</p>
-        </div>
-        <div className='sm:flex relative left-20 top-48'> 
-        <button className='sm:bg-[white] px-10 ps-5 py-2 rounded-full' type='button'>Book appointment</button>
-        <img src={assets.arrow_icon} className='sm:relative -left-7'  />
+    <div className="flex flex-col lg:flex-row items-center justify-between bg-[#5f6fff] rounded-xl mx-6 sm:mx-12 mt-6 px-6 sm:px-12 py-10">
+      
+      {/* Left Section */}
+      <section className="text-center lg:text-left max-w-xl">
+        <p className="text-3xl sm:text-5xl text-white font-semibold leading-snug">
+          Book Appointment <br /> With Trusted Doctors
+        </p>
 
+        <div className="flex items-center text-white mt-6 justify-center lg:justify-start">
+          <img src={assets.group_profiles} alt="" className="w-16 sm:w-auto" />
+          <p className="ms-4 text-sm sm:text-base">
+            Simply browse through our extensive list of trusted doctors, <br />
+            schedule your appointment hassle-free.
+          </p>
         </div>
 
+        <div className="flex items-center mt-8 justify-center lg:justify-start">
+          <button className="bg-white text-[#5f6fff] px-6 sm:px-10 py-2 rounded-full font-semibold hover:bg-gray-100 transition">
+            Book Appointment
+          </button>
+          <img src={assets.arrow_icon} alt="" className="ml-3 w-6 sm:w-8" />
+        </div>
       </section>
-      <section>
-        <img src={assets.header_img} className='sm:relative w-[500px] left-25 top-24   ' />
+
+      {/* Right Section */}
+      <section className="mt-10 lg:mt-0">
+        <img src={assets.header_img} alt="" className="w-[300px] sm:w-[400px] lg:w-[500px]" />
       </section>
     </div>
   )
