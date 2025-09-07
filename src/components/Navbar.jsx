499c0a51b9c9b9cbfe1e02229a2d854cce84a7ad
Mani-sh27 <beingahuman2512gmail.com>
2025-09-07 15:16:53 +0530

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             </NavLink>
             <div className='flex'>
               {
-                token ? <div>
+                token ? <div className=''>
                   <img src={assets.profile_pic} alt="" />
                   <img src={assets.dropdown_icon} alt="" />
                 </div> : <div className='hidden sm:block'>
