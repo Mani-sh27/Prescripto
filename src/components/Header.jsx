fe10b1b0efc1fbe2190ed6b939cbfae7bc63424d
Mani-sh27 <beingahuman2512gmail.com>
2025-09-08 16:42:50 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
           With Trusted Doctors</p>
         <div className='sm:flex relative top-40 left-20'>
           <img src={assets.group_profiles} className='relative left- sm:w-30' />
-          <p className=' ms- sm:text-white relative'>Simply browse through our extensive list of trusted doctors,<br />
+          <p className=' ms-20 sm:text-white relative'>Simply browse through our extensive list of trusted doctors,<br />
             schedule your appointment hassle-free.</p>
         </div>
         <div className='sm:flex relative left-20 top-48'> 
