15071cddfd2509e101e6383e137843176eb637f6
Mani-sh27 <beingahuman2512gmail.com>
2025-09-08 21:10:47 +0530

    auto commit

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -8,7 +8,7 @@ const SpecialityMenu = () => {
           <p>Find by Speciality</p>
           <p>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
       </section>
-       <section id='Speciality' className=' sm:flex gap-5 ms- text-center'>
+       <section id='Speciality' className=' sm:flex gap-5 ms-80 text-center'>
             { specialityData.map((item , index)=>(
               <Link key={index} to={`/doctors/${item.speciality}`}>
                   <img className='sm:size-20' src={item.image}  />
