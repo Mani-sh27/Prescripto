51676e21993ba6bb52d97313c27ebe599e1705d3
Mani-sh27 <beingahuman2512gmail.com>
2025-09-08 06:08:16 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
 
       </section>
       <section> 
-        <img src={assets.header_img} className='w-[970px] relative top-45 -left-1  sm:w-[500px] sm:top-40  sm:relative sm:left-18' />
+        <img src={assets.header_img} className='w-[970px] relative top-45 -left-1  sm:w-[500px] sm:top-0  sm:relative sm:left-18' />
       </section>
     </div>
   )
