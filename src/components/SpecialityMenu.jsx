2697d007269173411193efc1451bcca0f66f757b
Mani-sh27 <beingahuman2512gmail.com>
2025-09-08 18:38:07 +0530

    auto commit

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -12,7 +12,7 @@ const SpecialityMenu = () => {
             { specialityData.map((item , index)=>(
               <Link key={index} to={`/doctors/${item.speciality}`}>
                   <img src={item.image}  />
-                  <p>{}</p>
+                  <p>{DataTransferItemList.}</p>
               </Link>
               ))}
        </section>
