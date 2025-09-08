16be011d5ce08a341466a923e0ec45043cedabde
Mani-sh27 <beingahuman2512gmail.com>
2025-09-08 18:25:32 +0530

    auto commit

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -9,7 +9,7 @@ const SpecialityMenu = () => {
           <p>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
       </section>
        <section>
-            { specialityData.map((item.speciality)=>(
+            { specialityData.map(()=>(
               <Link></Link>
               ))}
        </section>
