f4ffdde57b296f8cd341c070eeb0af691de685f3
Mani-sh27 <beingahuman2512gmail.com>
2025-09-08 17:50:59 +0530

    auto commit

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { assets } from '../assets/assets_frontend/assets'
 const SpecialityMenu = () => {
   return (
-    <div></div>
+    <div>
+      
+    </div>
   )
 }
 
