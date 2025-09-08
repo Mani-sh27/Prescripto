19951585de7fa7549c4b5af3a275fd0b98e50995
Mani-sh27 <beingahuman2512gmail.com>
2025-09-08 15:44:42 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <section >
         <p className='sm:text-white sm:relative left-20 text-4xl top-30'>Book Appointment <br />
           With Trusted Doctors</p>
-        <div className='sm:flex '>
+        <div className='sm:flex text-2xl'>
           <img src={assets.group_profiles} className='' />
           <p className=''>Simply browse through our extensive list of trusted doctors,<br />
             schedule your appointment hassle-free.</p>
