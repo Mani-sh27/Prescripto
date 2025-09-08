f5d033dbf44f3421fd7d88e09725c14664ce15ff
Mani-sh27 <beingahuman2512gmail.com>
2025-09-08 15:49:59 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
           <p className=''>Simply browse through our extensive list of trusted doctors,<br />
             schedule your appointment hassle-free.</p>
         </div>
-        <div className='sm:flex relative left-30 top-48'> 
+        <div className='sm:flex relative left-20 top-48'> 
         <button className='sm:bg-[white] px-10 ps-5 py-2 rounded-full' type='button'>Book appointment</button>
         <img src={assets.arrow_icon} className=' w-10  sm:relative sm:w-5 cursor-pointer -left-7'  />
         </div>
