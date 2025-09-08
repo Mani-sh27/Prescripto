972e99fd77f48ab3abd5fb2048219d3ba516e497
Mani-sh27 <beingahuman2512gmail.com>
2025-09-08 05:41:49 +0530

    auto commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,6 @@ const Header = () => {
         <div className='sm:flex relative left-20 top-48'> 
         <button className='sm:bg-[white] px-10 ps-5 py-2 rounded-full' type='button'>Book appointment</button>
         <img src={assets.arrow_icon} className='sm:relative -left-7'  />
-
         </div>
 
       </section>
