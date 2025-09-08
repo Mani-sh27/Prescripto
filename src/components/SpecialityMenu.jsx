04fe883e192febe4ee3c4bed73663f6fdfcdbb12
Mani-sh27 <beingahuman2512gmail.com>
2025-09-08 18:26:53 +0530

    auto commit

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -10,7 +10,7 @@ const SpecialityMenu = () => {
       </section>
        <section id='Speciality'>
             { specialityData.map((item , index)=>(
-              <Link key={} >
+              <Link key={index} >
 
               </Link>
               ))}
