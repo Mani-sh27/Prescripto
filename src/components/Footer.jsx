27fce6e14402c0229165eb1a2acf9a560f972b23
Mani-sh27 <beingahuman2512gmail.com>
2025-09-10 00:07:14 +0530

    auto commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
         </div>
 
       </div>
-      <hr className='border-t-1 text-center' />
+      <hr className='border-t-1 mt-' />
       <p>Copyright 2024 @ Greatstack.dev - All Right Reserved.</p>
     </>
 
