223ce9f5bc7e5154838a112f5522c616319c5d43
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 20:53:46 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='w-[20px]'>
+    <div className='w-[200px]'>
       <section>
         <p>Book Appointment</p>
       </section>
