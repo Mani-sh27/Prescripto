1c3e47bf70c5bdccd74826db8095a8b7300043b0
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 22:49:59 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -19,7 +19,7 @@ const Doctors = () => {
           ))
         }
       </div>
-      <button className='sm:relative '>More</button>
+      <button className='sm:relative flex '>More</button>
     </div>
   )
   
