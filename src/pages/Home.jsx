aa06943c011a37bd49b4af3cc5744372bfc0f355
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 15:25:32 +0530

    auto commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Header } from "../components/Header";
-import {SpecialityMenu} from "../components/SpecialityMenu"
+import {SpecialityMenu} from "../components/SpecialityMenu";
+import
 const Home = () => {
   return (
     <div> <Header />
