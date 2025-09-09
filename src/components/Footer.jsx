5beda8e7365578ca3c865c8e0d59a3d3eddda3c3
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 23:08:50 +0530

    auto commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,9 @@ const Footer = () => {
         <img  className='' src={assets.logo}  />
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
       </div>
+      <div>
+        
+      </div>
     </div>
   )
 }
