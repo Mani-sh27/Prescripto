1c1a681336ec453609c1ff46f28005e80181f1eb
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 15:31:09 +0530

    auto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
