0fad5ee642e5db0b74f0de0dda224f8d90296161
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 15:50:17 +0530

    auto commit

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -9,7 +9,7 @@ const Doctors = () => {
         {
           doctors.slice(0,10).map((item,index)=>(
               <div>
-                <img src={item.im} alt="" />
+                <img src={item.image} alt="" />
               </div>
           ))
         }
