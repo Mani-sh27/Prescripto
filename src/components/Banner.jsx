8b9c89d2ee01a95169703e0b8ed8b5a07e8b61a1
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 22:16:17 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
         <p>Book Appointment</p>
         <p>With 100+ Trusted Doctors</p>
       </section>
-        <button className='text-white text'>Create Account</button>
+        <button className='text-white text-2xl'>Create Account</button>
       <section>
         <img className='sm:w-xs me-20 hidden  ' src={assets.appointment_img}  />
       </section>
