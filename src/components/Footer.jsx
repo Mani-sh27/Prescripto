65900c6e058d285459ee34ad4a0998f291f0e7d8
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 23:14:45 +0530

    auto commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets_frontend/assets'
 
 const Footer = () => {
   return (
-    <div className='border-4 w-[1240px] mt- gap-38 ms-32 grid grid-cols-3'>
+    <div className='border-4 w-[1240px] mt-48 gap-38 ms-32 grid grid-cols-3'>
       <div className=''>
         <img  className='' src={assets.logo}  />
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
