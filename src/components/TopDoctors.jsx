55506d51894b8b4911cb2c8dd145abaf1269fffd
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 16:27:53 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -9,7 +9,7 @@ const Doctors = () => {
       <div className=' sm: gap-5 grid w-[1240px] mt-10 relative left-32 grid-cols-5'>
         {
           doctors.slice(0,10).map((item,index)=>(
-              <div className='sm:border-1 rounded-xl border-blue-200 shadow-xl '>
+              <div className='sm: rounded-xl border-blue-200 shadow-xl '>
                 <img src={item.image} alt="" />
                 <div>
                   <p className=''>Available</p>
