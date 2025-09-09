d48ff6b2cd885a2271895c095755e226bc89158d
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 21:44:24 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets_frontend/assets'
 const Banner = () => {
   return (
     <div className=' flex relative bg-[#5f6fff] justify-between left-64 -ms-32 w-[1240px]'>
-      <section className='text-white text-4xl font-semibold mt-'>
+      <section className='text-white text-4xl font-semibold mt-2'>
         <p>Book Appointment</p>
         <p>With 100+ Trusted Doctors</p>
       </section>
