31a85390c9e878334961b66e1cf8bd5ba3e2a228
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 15:29:56 +0530

    auto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
-
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'
 import Login from './pages/Login'
