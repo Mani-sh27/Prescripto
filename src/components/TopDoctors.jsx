be8b0f334f84d6092fab9be47ccfc2154c487191
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 22:58:52 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -19,7 +19,7 @@ const Doctors = () => {
           ))
         }
       </div>
-      <button className='sm:flex  bg-gray-200 my-16 py-4 px-16 ms-160 curso rounded-full'>More</button>
+      <button className='sm:flex  bg-gray-200 my-16 py-4 px-16 ms-160 cursor-pointer rounded-full'>More</button>
     </div>
   )
   
