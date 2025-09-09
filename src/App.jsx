f8590a77556e608e711ac406d8fbf2893e3782f3
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 15:23:10 +0530

    auto commit

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         <Navbar />
          
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
 
 
         <Route path='/doctors' element={<Doctors />} />
