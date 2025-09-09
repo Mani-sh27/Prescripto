1b4517b76746564da5557ce89e2c3247ca8e5921
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 15:59:31 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { doctors } from '../assets/assets_frontend/assets';
 
-const TopDoctors = () => {
+const Doctors = () => {
   return (
-    <div></div>
+    <div>
+      <p>Top Doctors</p>
+      <p>Simply browse through our extensive list of trusted doctors.</p>
+      <div>
+        {
+          doctors.slice(0,10).map((item,index)=>(
+              <div>
+                <img src={item.image} alt="" />
+              </div>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
-export default TopDoctors
+export default Doctors
