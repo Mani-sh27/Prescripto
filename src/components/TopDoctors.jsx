1dd702406a08c098b3e54b8be2a1fadf8f6debe9
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 18:01:00 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -11,7 +11,7 @@ const Doctors = () => {
               <div className='rounded-2xl  transition-all hover:-translate-y-2 cursor-pointer border-blue-200 shadow-sm '>
                 <img className='bg-blue-50 rounded-t-2xl' src={item.image}  /> 
                 <div>
-                  <p className='w-2 h-2 rounded-full'></p><p className='my-3 ms-7'>Available</p>
+                  <p className='w-2 h-2 rounded-full '></p><p className='my-3 ms-7'>Available</p>
                   <p className='-mt-3 ms-3 text-md font-semibold'>{item.name}</p>
                   <p className='text-xs ms-3 mb-3 text-gray-500'>{item.speciality}</p>
                 </div>
