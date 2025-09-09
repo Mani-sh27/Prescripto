f710ffbab101bb71c58d4c9ee920494a87762113
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 16:24:11 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -9,7 +9,7 @@ const Doctors = () => {
       <div className=' sm:grid w-[1240px] mt-10 relative left-32 grid-cols-5'>
         {
           doctors.slice(0,10).map((item,index)=>(
-              <div className='border-1'>
+              <div className='border-1 '>
                 <img src={item.image} alt="" />
                 <div>
                   <p className=''>Available</p>
