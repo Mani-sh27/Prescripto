ff1a10897e92f895db42a25a1526964e8bcf8218
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 18:14:57 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,13 +2,7 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>
-      <div>
-        <p></p>
-        <p></p>
-      </div>
-      <button>Create account</button>
-    </div>
+    
   )
 }
 
