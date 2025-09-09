3fe4c36a38a5ba8e0d14f867a88edbd5f385b5c8
Mani-sh27 <beingahuman2512gmail.com>
2025-09-10 00:06:18 +0530

    auto commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import { assets } from '../assets/assets_frontend/assets'
 
 const Footer = () => {
   return (
-    
+    <>
+    </>
     <div className='sm:w-[1240px] w-[860px] flex-col flex gap-16 mt-48 sm:gap-0 ms-32 sm:grid grid-cols-3'>
       <div className=''>
         <img  className='w-44' src={assets.logo}  />
