ab28ed8363d7a8fc7f4d562678286434a71cd3d4
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 07:16:01 +0530

    auto commit

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -5,7 +5,7 @@ const SpecialityMenu = () => {
   return (
     <div>
       <section className=' relative top-16 sm:relative sm:top-4'>
-          <p className='text-center relative left-48  text-5xl  sm:relative top-10 sm:-left-8 sm:text-3xl font-semibold '>Find by Speciality</p>
+          <p className='text-center relative left-48  text-5xl  sm:relative top-10 sm:-left-16 sm:text-3xl font-semibold '>Find by Speciality</p>
           <p className='relative top-20 text-center sm:relative sm:text-center text-sm  sm:top-20  '>Simply browse through our extensive list of trusted doctors,<br />schedule your appointment hassle-free.</p>
       </section>
        <section id='Speciality' className=' overflow-x-scroll relative top-12 left-60 flex justify-center sm:flex sm:relative sm:-left-1 gap-5 sm:justify-center mt-20 text-center'>
