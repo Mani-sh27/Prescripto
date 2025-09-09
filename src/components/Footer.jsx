a1e66777cb948d55b77b5809bb5cca3ea7fff7e3
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 23:03:56 +0530

    auto commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { assets } from '../assets/assets_frontend/assets'
 
 const Footer = () => {
   return (
     <div>
       <div>
-        <img src={} alt="" />
+        <img src={assets} alt="" />
       </div>
     </div>
   )
