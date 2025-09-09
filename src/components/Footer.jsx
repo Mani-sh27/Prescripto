976844beba9e7e5b3f3fff4560d98d87c9e782e6
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 23:07:08 +0530

    auto commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='border-4 w-[1240px] ms-32'>
       <div className=''>
-        <img src={assets.logo}  />
+        <img   src={assets.logo}  />
       </div>
     </div>
   )
