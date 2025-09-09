f8c037b3a21c12e86eba107ba4f1ad894dc32e8c
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 16:21:49 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -4,7 +4,7 @@ import { assets, doctors } from '../assets/assets_frontend/assets';
 const Doctors = () => {
   return (
     <div>
-      <p >Top Doctors</p>
+      <p te>Top Doctors</p>
       <p>Simply browse through our extensive list of trusted doctors.</p>
       <div className=' sm:grid w-[1240px] mt-20 relative left-32 grid-cols-5'>
         {
