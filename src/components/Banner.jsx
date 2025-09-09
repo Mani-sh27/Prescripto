18ed754b688bd2bac6437fe64366e3b620ac0212
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 20:53:34 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div className='w-['>
+    <div className='w-[]'>
       <section>
         <p>Book Appointment</p>
       </section>
