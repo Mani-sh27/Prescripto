ca25d6f175137d74c9acce4ef9bcc5d5992bbcf8
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 15:24:57 +0530

    auto commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Header } from "../components/Header";
-import { specialityData } from '../assets/assets_frontend/assets';
-
+import {sp}
 const Home = () => {
   return (
     <div> <Header />
