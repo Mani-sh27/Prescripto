acfd416c536a4f0768bb26d9864f001d603ff622
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 22:27:36 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
         <p>With 100+ Trusted Doctors</p>
       </section>
       <div>
-        <button className='sm:text-2xl relative top-8 -left-110  bg-white sm:top-52'>Create Account</button>
+        <button className='sm:text-2xl relative top-8 -left-110  bg-white sm:top-52 '>Create Account</button>
       </div>
       <section>
         <img className='sm:w-xs me-20  sm:flex hidden ' src={assets.appointment_img}  />
