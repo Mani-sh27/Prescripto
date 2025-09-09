d01bb93283874e05cdda0bdfb08849c076db250a
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 16:32:10 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -14,7 +14,7 @@ const Doctors = () => {
                 <div>
                   <p className='my-3 ms-3'>Available</p>
                   <p className='-mt-3 ms-3'>{item.name}</p>
-                  <p className='text-sm'>{item.speciality}</p>
+                  <p className='text-xm'>{item.speciality}</p>
                 </div>
               </div>
           ))
