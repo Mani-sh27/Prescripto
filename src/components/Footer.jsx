f1bbda711d83b15611753420dea696bbabd006b2
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 23:23:22 +0530

    auto commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='border-4 w-[1240px] mt-68 gap-0 ms-32 grid grid-cols-3'>
       <div className=''>
         <img  className='' src={assets.logo}  />
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        <p>lorem</p>
       </div>
       <div className='ms-48'>
         <h2>COMPANY</h2>
