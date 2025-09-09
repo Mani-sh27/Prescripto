f8a3bd27a35651e055462445274f22487998d991
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 21:57:31 +0530

    auto commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
       </section>
         <button>Create Account</button>
       <section>
-        <img className='sm:w-xs me-20 hidden ' src={assets.appointment_img}  />
+        <img className='sm:w-xs me-20 hidden  ' src={assets.appointment_img}  />
       </section>
     </div>
   )
