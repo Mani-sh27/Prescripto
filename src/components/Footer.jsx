904b8ab4ed8e1773bd9988279bef64fa6768716e
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 23:05:11 +0530

    auto commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets_frontend/assets'
 
 const Footer = () => {
   return (
-    <div className='border-4 '>
+    <div className='border-4 w-'>
       <div className=''>
         <img src={assets.logo} alt="" />
       </div>
