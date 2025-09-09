0b18dd12ffb2e57a3439a098e929346bc6058356
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 17:14:24 +0530

    auto commit

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -3,7 +3,7 @@ import { assets, specialityData } from '../assets/assets_frontend/assets'
 import { Link } from 'react-router-dom'
 const SpecialityMenu = () => {
   return (
-    <div className='-ms-'>
+    <div className='-ms-5'>
       <section className='relative top-16  sm:relative left-8 sm:top-4'>
         <p className='text-center relative left-48  text-5xl  sm:relative top-10 sm:-left-18  sm:text-3xl font-semibold '>Find by Speciality</p>
         <p className='relative left-48 text-wrap top-20 text-center sm:relative sm:text-center sm:-left-18 text-sm sm:top-20  '>Simply browse through our extensive list of trusted doctors,<br />schedule your appointment hassle-free.</p>
