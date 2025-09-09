0409a50fb0b40b241376f26ede648e5471c7b154
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 23:32:33 +0530

    auto commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       </div>
       <div className='ms-48'>
         <h2 className='text-2xl font-medium'>COMPANY</h2>
-        <ul className='mt-8 text-gray-600 tex'>
+        <ul className='mt-8 text-gray-600 text-'>
           <li>Home</li>
           <li>About us</li>
           <li>Delivery</li>
