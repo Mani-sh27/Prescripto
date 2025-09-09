135eb3544508cc53df1558393c74f87c16d4b4a4
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 16:06:24 +0530

    auto commit

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -12,7 +12,7 @@ const Doctors = () => {
               <div>
                 <img src={item.image} alt="" />
                 <div>
-                  <p>Ava</p>
+                  <p>Available</p>
                 </div>
               </div>
           ))
