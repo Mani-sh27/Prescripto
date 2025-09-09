7136d3a06afe5a84dac7272aa2df6d9253bec047
Mani-sh27 <beingahuman2512gmail.com>
2025-09-09 15:43:07 +0530

    auto commit

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -7,7 +7,7 @@ const Doctors = () => {
       <p>Simply browse through our extensive list of trusted doctors.</p>
       <div>
         {
-          doctors.slice(0,10).ma
+          doctors.slice(0,10).map
         }
       </div>
     </div>
