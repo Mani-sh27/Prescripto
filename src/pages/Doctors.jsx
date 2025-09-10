8e6196242bbaa1418b2e82199af2ef3911f63ca3
Mani-sh27 <beingahuman2512gmail.com>
2025-09-10 17:57:56 +0530

    auto commit

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom';
-import 
+import {}
 const Doctors = () => {
   const { speciality } = useParams()
   const { doctors } = useContext(App)
